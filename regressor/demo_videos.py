47c7b9e7ff7d5b901ac412b0b11fac0bf28278e1
nagabhushan <nagabhushan@spreeai.com>
2024-06-07 00:04:11 +0530

    HPSL003: demo_videos.py updated to symlink images and keypoints to a tmp directory, which is then fed to demo.py.

--- regressor/demo_videos.py
+++ regressor/demo_videos.py
@@ -1,12 +1,13 @@
 # Calls demo for every video iteratively
 
+import argparse
 import copy
 import datetime
+import os
+import shutil
 import time
 import traceback
-
 from pathlib import Path
-import argparse
 
 from demo import call_main
 
@@ -14,19 +15,61 @@ this_filepath = Path(__file__)
 this_filename = this_filepath.stem
 
 
+def execute_shell_command(cmd: str):
+    print(cmd)
+    os.system(cmd)
+    return
+
+
+def delete_directory(dirpath: Path):
+    if dirpath.exists():
+        shutil.rmtree(dirpath)
+    return
+
+
+def clean_directory(dirpath: Path):
+    delete_directory(dirpath)
+    dirpath.mkdir(parents=True, exist_ok=True)
+    return
+
+
 def main():
     args = parse_args()
+
+    tmp_dirpath = Path('../../tmp')
     videos_dirpath = Path(args.videos_dirpath)
+    keypoints_path_format = args.keypoints_path_format
     output_dirpath = Path(args.output_dirpath)
+
     for video_frames_dirpath in sorted(videos_dirpath.iterdir()):
         video_name = video_frames_dirpath.stem
+        video_output_path = output_dirpath / f'{video_name}/{video_name}.mp4'
+        if video_output_path.exists():
+            continue
+
+        clean_directory(tmp_dirpath)
+        tmp_images_dirpath = tmp_dirpath / 'images'
+        tmp_keypoints_dirpath = tmp_dirpath / 'keypoints'
+        tmp_images_dirpath.mkdir(parents=True, exist_ok=True)
+        tmp_keypoints_dirpath.mkdir(parents=True, exist_ok=True)
+        for frame_path in sorted(video_frames_dirpath.iterdir()):
+            frame_num = int(frame_path.stem)
+            keypoints_path = Path(keypoints_path_format.format(video_name=video_name, frame_num=frame_num))
+            tmp_frame_path = tmp_images_dirpath / f'{frame_num:04}.png'
+            tmp_keypoints_path = tmp_keypoints_dirpath / f'{frame_num:04}.json'
+            frame_path.symlink_to(tmp_frame_path)
+            keypoints_path.symlink_to(tmp_keypoints_path)
+
         video_output_dirpath = output_dirpath / f'{video_name}/frames'
         video_output_dirpath.mkdir(parents=True, exist_ok=True)
         demo_args = copy.deepcopy(args)
         del demo_args.videos_dirpath
+        del demo_args.keypoints_path_format
         del demo_args.output_dirpath
         demo_args.output_folder = video_output_dirpath.as_posix()
-        demo_args.exp_opts.append(f'datasets.pose.openpose.data_folder={video_frames_dirpath.absolute().as_posix()}')
+        demo_args.exp_opts.append(f'datasets.pose.openpose.data_folder={tmp_dirpath.as_posix()}')
+        demo_args.exp_opts.append(f'datasets.pose.openpose.img_folder={tmp_images_dirpath.stem}')
+        demo_args.exp_opts.append(f'datasets.pose.openpose.keyp_folder={tmp_keypoints_dirpath.stem}')
         call_main(demo_args)
     return
 
@@ -42,7 +85,10 @@ def parse_args():
                         help='The configuration of the experiment')
     parser.add_argument('--videos-dirpath', dest='videos_dirpath',
                         default='../../../../../../databases/spree_internal/data/rgb_png', type=str,
-                        help='The folder where the renderings will be saved')
+                        help='The path to directory containing videos (frames)')
+    parser.add_argument('--keypoints-path-format', dest='keypoints_path_format', type=str,
+                        default='../../../../research/001_VitPose_halpe/runs/testing/test0001/{video_name}/json/{frame_num:04}.json',
+                        help='The formatted path to directory containing 2D keypoints (video_name and frame_num will be inserted)')
     parser.add_argument('--output-dirpath', dest='output_dirpath',
                         default='../runs/testing/test0000', type=str,
                         help='The folder where the renderings will be saved')
