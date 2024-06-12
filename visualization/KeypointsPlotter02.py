07c1784fd971af93c0198d27b0f64477561581f9
nagabhushan <nagabhushan@spreeai.com>
2024-06-12 16:41:06 +0530

    HPSL003: KeypointsPlotter02.py added.

--- visualization/KeypointsPlotter02.py
+++ visualization/KeypointsPlotter02.py
@@ -1,4 +1,5 @@
-# Plots the SMPL-X joints on top of image
+# Plots projected joints on rendered mesh
+# Uses the mesh renderer from Phong's code
 
 import datetime
 import time
@@ -25,6 +26,81 @@ from scipy.spatial.transform import Rotation
 
 this_filepath = Path(__file__)
 this_filename = this_filepath.stem
+
+
+class Renderer(object):
+    def __init__(self, focal_length=600, img_w=512, img_h=512, camera_center=None, faces=None, same_mesh_color=False):
+        os.environ['PYOPENGL_PLATFORM'] = 'osmesa'
+        self.renderer = pyrender.OffscreenRenderer(viewport_width=img_w, viewport_height=img_h, point_size=1.0)
+        if camera_center is not None:
+            self.camera_center = camera_center
+        else:
+            self.camera_center = [img_w // 2, img_h // 2]
+        self.focal_length = focal_length
+        self.faces = faces
+        self.same_mesh_color = same_mesh_color
+        return
+
+    def render_front_view(self, verts, bg_img_rgb=None, bg_color=(0, 0, 0, 0)):
+        # Create a scene for each image and render all meshes
+        scene = pyrender.Scene(bg_color=bg_color, ambient_light=np.ones(3) * 0)
+        # Create camera. Camera will always be at [0,0,0]
+        camera = pyrender.camera.IntrinsicsCamera(fx=self.focal_length, fy=self.focal_length,
+                                                  cx=self.camera_center[0], cy=self.camera_center[1])
+        scene.add(camera, pose=np.eye(4))
+
+        # Create light source
+        light = pyrender.DirectionalLight(color=[1.0, 1.0, 1.0], intensity=3.0)
+        # for DirectionalLight, only rotation matters
+        light_pose = trimesh.transformations.rotation_matrix(np.radians(-45), [1, 0, 0])
+        scene.add(light, pose=light_pose)
+        light_pose = trimesh.transformations.rotation_matrix(np.radians(45), [0, 1, 0])
+        scene.add(light, pose=light_pose)
+
+        # Need to flip x-axis
+        rot = trimesh.transformations.rotation_matrix(np.radians(180), [1, 0, 0])
+        # multiple person
+        num_people = len(verts)
+        # for every person in the scene
+        for n in range(num_people):
+            mesh = trimesh.Trimesh(verts[n], self.faces)
+            mesh.apply_transform(rot)
+            if self.same_mesh_color:
+                mesh_color = colorsys.hsv_to_rgb(0.6, 0.5, 1.0)
+            else:
+                mesh_color = colorsys.hsv_to_rgb(float(n) / num_people, 0.5, 1.0)
+            material = pyrender.MetallicRoughnessMaterial(
+                metallicFactor=0.2,
+                alphaMode='OPAQUE',
+                baseColorFactor=mesh_color)
+            mesh = pyrender.Mesh.from_trimesh(mesh, material=material, wireframe=False)
+            scene.add(mesh, 'mesh')
+
+        # Alpha channel was not working previously, need to check again
+        # Until this is fixed use hack with depth image to get the opacity
+        color_rgba, depth_map = self.renderer.render(scene, flags=pyrender.RenderFlags.RGBA)
+        color_rgb = color_rgba[:, :, :3]
+        if bg_img_rgb is None:
+            return color_rgb
+        else:
+            mask = depth_map > 0
+            bg_img_rgb[mask] = color_rgb[mask]
+            return bg_img_rgb
+
+    def render_side_view(self, verts):
+        centroid = verts.mean(axis=(0, 1))  # n*6890*3 -> 3
+        # make the centroid at the image center (the X and Y coordinates are zeros)
+        centroid[:2] = 0
+        aroundy = cv2.Rodrigues(np.array([0, np.radians(90.), 0]))[0][np.newaxis, ...]  # 1*3*3
+        pred_vert_arr_side = np.matmul((verts - centroid), aroundy) + centroid
+        side_view = self.render_front_view(pred_vert_arr_side)
+        return side_view
+
+    def delete(self):
+        """
+        Need to delete before creating the renderer next time
+        """
+        self.renderer.delete()
 
 
 def perspective_projection(points, rotation, translation, intrinsics):
@@ -121,17 +197,25 @@ def main():
     smplx_model = SMPLXLayer(models_dirpath.as_posix(), num_betas=num_shape_params)
     smplx_output = smplx_model(betas=shape_params_tr, body_pose=pose_matrices_tr[:, 1:], global_orient=pose_matrices_tr[:, :1], pose2rot=False)
 
+    faces = smplx_model.faces
+    raw_vertices = smplx_output.vertices
+    translated_vertices = raw_vertices + camera_translation
+
+    renderer = Renderer(focal_length=focal_length, img_w=w, img_h=h, camera_center=camera_center, faces=faces, same_mesh_color=False)
+    front_view = renderer.render_front_view(translated_vertices.cpu().numpy(), bg_img_rgb=image.copy())
+
     joints3d = smplx_output.joints[0]
     joints2d = perspective_projection(joints3d.cpu().numpy(), rotation=numpy.eye(3),
                                       translation=camera_translation, intrinsics=camera_intrinsics)
 
-    pyplot.imshow(image)
+    pyplot.figure(figsize=(19.20, 10.80))
+    pyplot.imshow(front_view)
     for joint_index, joint_location in enumerate(joints2d):
         x, y = joint_location
         pyplot.scatter(x, y, c='r', s=8)
         if show_keypoint_index:
             pyplot.text(x, y, str(joint_index), fontsize=8, color='r')
-    pyplot.show()
+    pyplot.savefig('JointsOnMesh.png')
     return
 
 
